49919a4e91194e7dbbc17e46181f94f377b418a7
Sneha <snyadav@iu.edu>
2025-10-03 11:58:12 -0400

    commenting

--- apputil.py
+++ apputil.py
@@ -4,19 +4,22 @@ import pandas as pd
 class Genius:
 
     def __init__(self, access_token):
-
+        """ Initialize the Genius class with an access token. """
         self.access_token = access_token
 
     def get_access_token(self):
+        """ Return the access token. """
         return self.access_token
 
     def get_artist(self, search_term):
+        """ Search for an artist by name and return their details. """
         genius_search_url = f"http://api.genius.com/search?q={search_term}&access_token={self.access_token}"
         response = requests.get(genius_search_url)
         json_data = response.json()
         return json_data['response']['hits']
     
     def get_artists(self, search_terms):
+        """ Search for multiple artists and return their details in a DataFrame. """
         search_results = []
         for term in search_terms:
             details = self.get_artist(term)
